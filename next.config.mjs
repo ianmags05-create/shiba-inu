594f32407f7419a7d8932e193481cac39eb1eaa4
ianmags05-create <ianmags05@gmail.com>
2026-09-14 16:44:27 +0800

    Allow Hostinger server action requests

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "khaki-meerkat-385691.hostingersite.com",
+        "shibainupetshop.com",
+        "www.shibainupetshop.com",
+      ],
+    },
+  },
   async rewrites() {
     return {
       beforeFiles: [
